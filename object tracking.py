89516040a7f96b80ccb0a6ccb52abf6a92a696c1
Olesens <saraholesen@hotmail.com>
2018-09-18 18:09:39 +0100

    yaya

--- object tracking.py
+++ object tracking.py
@@ -9,6 +9,40 @@ import math
 import time
 from picamera.array import PiRGBArray
 from picamera import PiCamera
+from gpiozero import CamJamKitRobot, DistanceSensor
+from coloer_detect import DetectBlue
+
+#from Obstacle_Avoidance
+
+print("Hey there")
+
+camera = PiCamera ()
+camera.resolution = (160, 120)
+camera.framerate = 16
+rawCapture = PiRGBArray(camera, size =(160,120))
+time.sleep(0.1)
+
+robot=CamJamKitRobot()
+hownear=15.0
+reversetime=0.5
+turntime=0.75
+
+leftmotorspeed=0.5
+rightmotorspeed=0.5
+
+motorforward=(leftmotorspeed, rightmotorspeed)
+motorbackward=(-leftmotorspeed, -rightmotorspeed)
+motorleft=(leftmotorspeed,0)
+motorright=(0,rightmotorspeed)
+
+
+####our color detect function
+import numpy as np
+import cv2
+import math
+import time
+from picamera.array import PiRGBArray
+from picamera import PiCamera
 
 camera = PiCamera ()
 camera.resolution = (320, 240)
@@ -80,67 +114,24 @@ def DetectBlue ():
     cv2.destroyAllWindows()
 
     camera.destroy()
+###########################################################
 
 
-#while(1):
 
-    # Take each frame
- #   ret, frame = cap.read()
+while True:
+    print("hi")
+    DetectBlue()
+    print(cX)
+    if cX !=150:
+        robot.value=motorforward
+    else:
+        robot.value=motorbackward
+#try:
+    #if isnearobstacle(hownear):
+        #robot.stop()
+       # avoidobstacle()
+        #except 
 
-    #could potentially need a step of blurring the image (GaussianBlur)
-  #  blurred = cv2.medianBlur(frame, 5)
+#cv2.destroyAllWindows()
 
-    # Convert BGR to HSV
-   # hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
-
-    # define range of red color in HSV
-   # lower_red = np.array([100,100,100])
-    #upper_red = np.array([130,255,255])
-
-    # Threshold the HSV image to get only red colors
-   # mask = cv2.inRange(hsv, lower_red, upper_red)
-
-    # Bitwise-AND mask and original image
-    #res = cv2.bitwise_and(frame,frame, mask = mask)
-
-    #do I need a threshold here to make the image binary?
-    #ret,thresh = cv2.threshold(mask, 40, 255, 0)
-
-    #find contours in video
-    #consider adding a "round" criteria for optimization
-    #contours = cv2.findContours(mask, cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)[-2]
-   # for c in contours:
-        #cv2.drawContours(frame, [c], -1, (0,255,0),3)
-
-        #display resulting frame
-       # cv2.imshow('frame',frame)
-
-        # find the biggest area and put a green square around it
-        #c_m = max(contours, key=cv2.contourArea)
-        #x, y, w, h = cv2.boundingRect(c_m)
-        #cv2.rectangle(res, (x, y), (x + w, y + h), (0, 255, 0), 2)
-
-        #report the centroid of the biggest contour
-       # M = cv2.moments(c_m)
-       # if M["m00"] != 0:
-         #   cX = int(M["m10"] / M["m00"])
-          #  cY = int(M["m01"] / M["m00"])
-          #  print("cx=", cX, "cy=", cY)
-        #this bit is neccesary for when object is detected
-       # else:
-          #  cX, cY = 0, 0
-           # print("No centroid found")
-
-       # M = 0
-
-       # if cv2.waitKey(1) & 0xFF == ord('q'):
-           # break
-
-    # show the images
-   # cv2.imshow('frame',frame)
-   # cv2.imshow('mask',mask)
-   # cv2.imshow('res',res)
-
-   # k = cv2.waitKey(5) & 0xFF
-   # if k == 27:
-       # break
+#camera.destroy()
