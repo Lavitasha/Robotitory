ea38e09a79cf8f5d0026e5b3133b304f2365eb0b
Olesens <saraholesen@hotmail.com>
2018-09-20 14:58:47 +0100

    Tried to adapt obstcle avoidance also added if stuck motions

--- Movegirl_obstacles.py
+++ Movegirl_obstacles.py
@@ -22,11 +22,16 @@ motorleft=(0,rightmotorspeed)
 # Distance and motor Variables
 robot=CamJamKitRobot()
 hownear=25.0
-reversetime=0.5
+reversetime=0.75
 turntime=0.75
 pinTrigger = 17
 pinEcho = 18
 sensor = DistanceSensor(echo=pinEcho, trigger=pinTrigger)
+
+## Initialize a variable for the explore function
+last_seen_balloon = 0
+time_turned = time.time()  
+turning_left = True
 
 
 #  Camera settings #
@@ -37,7 +42,7 @@ rawCapture = PiRGBArray(camera, size =(160,120))
 camera.rotation=180 
 
 #  Color to detect settings
-lower_red = np.array([100,100,100])
+lower_red = np.array([100,50,50])
 upper_red = np.array([130,255,255])
 
 
@@ -51,16 +56,6 @@ def MoveGirl(direction, speed=1):
     rightspeed = (1-direction)/2
     leftspeed = 1-rightspeed
     robot.value=(leftspeed * speed, rightspeed * speed)
-
-def Stuck():
-    robot.value=motorbackward
-    time.sleep(1)
-    robot.value=motorright
-    time.sleep(1)
-    robot.value=motorforward
-    time.sleep(0.5)
-
-
 
 def Avoidobstacle():
     print("Backwards")
@@ -81,16 +76,47 @@ def Nearobstacle(localhownear):
         return True
     else:
         print("Path Clear")
-        return False 
+        return False
 
-## Initialize a variable for the explore function
-last_seen_balloon = 0
-time_turned = time.time()  
-turning_left = True
+
+def Stuck():
+    print("Stuck")
+    robot.stop()
+    robot.value=motorbackward
+    time.sleep(reversetime)
+    robot.stop()
+    print("Left")
+    robot.value=motorleft
+    time.sleep(turntime)
+    robot.stop()
+
+
+######### The main loop! ###############
+frame_previous = 0
+wigglelike = 0
 
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
     frame = frame.array
 
+    # Stuck
+    if np.any(frame_previous):
+        frameDiff = cv2.absdiff(frame_previous, frame)
+        change = frameDiff.mean()
+        print("The difference is:",change)
+        if change > 1.5:
+            wigglelike = 0
+            moving = True
+            print("We are moving")
+        else:
+            wigglelike += 1
+            moving = False
+            if wigglelike > 5:
+                print("We are stuck")
+                Stuck()
+                
+            
+    frame_previous = frame
+    
     # Apply different mask the threshold according to color settings
     blurred = cv2.medianBlur(frame, 5)
     hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
@@ -113,16 +139,19 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
         print("Area is",area)
 
 
-        # Check if we are near an obstacle
-        while Nearobstacle(hownear):
-            if area > 5000:
+
+
+        # Check if we are near an obstacle if not blue = Avoid
+        if Nearobstacle(hownear):
+            if area > 2000:
                 print("We are near an obstacle but it is blue!")
-                break
-            if area < 5000:
+                    
+            else:
                 print("There is an obstacle in the way! Avoid!!")
                 Avoidobstacle()
-        
-        if (M["m00"] != 0) and (area > 100):
+
+            
+        elif (M["m00"] != 0) and (area > 100):
             print("Blue Ballon detected")
             cX = int(M["m10"] / M["m00"])
             cY = int(M["m01"] / M["m00"])
@@ -133,28 +162,29 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
             turn_amount = offset / 160
             MoveGirl(turn_amount, speed = 1)
             last_seen_balloon = time.time()
-               
-
+       
+        
+        
         else:
-            # might have to have a setting here defining cX and cY if you cant see anything
-            # or something checking if the pixels have changed a lot
-            # Explore()
-            # drive randomly
+           # Explore
             if time.time() - time_turned > 2:
                 turning_left = not turning_left
                 time_turned = time.time()
-                
-            if turning_left:
-                MoveGirl(-0.5, 0.5) ##actually turn left
+                            
+            # Explore functions
+            if turning_left:                     
+                MoveGirl(-0.5, 0.5)
+                MoveGirl(0, 0.25)
                 print("Exploring to the left")
+
             else:
-                MoveGirl(0.5, 0.5)            
+                MoveGirl(0.5, 0.5)
+                MoveGirl(0, 0.25)
                 print("Exploring to the right")
 
-        ###When Cx and Cy haven't change in x seconds enable "wiggle"
+             
             
-            
-        #M = 0          
+        # The end   
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
             robot.stop()
