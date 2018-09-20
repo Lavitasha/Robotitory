19a910e2a0a5ea5aa5b08b8c698b65f4db0ccb6d
Olesens <saraholesen@hotmail.com>
2018-09-20 16:40:18 +0100

    changes

--- Movegirl_obstacles.py
+++ Movegirl_obstacles.py
@@ -23,7 +23,7 @@ motorleft=(0,rightmotorspeed)
 robot=CamJamKitRobot()
 hownear=25.0
 reversetime=0.75
-turntime=0.75
+turntime=0.85
 pinTrigger = 17
 pinEcho = 18
 sensor = DistanceSensor(echo=pinEcho, trigger=pinTrigger)
@@ -42,7 +42,7 @@ rawCapture = PiRGBArray(camera, size =(160,120))
 camera.rotation=180 
 
 #  Color to detect settings
-lower_red = np.array([100,50,50])
+lower_red = np.array([100,75,75])
 upper_red = np.array([130,255,255])
 
 
@@ -94,6 +94,7 @@ def Stuck():
 ######### The main loop! ###############
 frame_previous = 0
 wigglelike = 0
+timeObstacleSeen = 0
 
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
     frame = frame.array
@@ -102,17 +103,18 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
     if np.any(frame_previous):
         frameDiff = cv2.absdiff(frame_previous, frame)
         change = frameDiff.mean()
-        print("The difference is:",change)
+        #print("The difference is:",change)
         if change > 1.5:
             wigglelike = 0
             moving = True
-            print("We are moving")
+            #print("We are moving")
         else:
             wigglelike += 1
             moving = False
             if wigglelike > 5:
                 print("We are stuck")
-                Stuck()
+                MoveGirl(0.5, -1)
+                time.sleep(0.2)
                 
             
     frame_previous = frame
@@ -142,16 +144,16 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
 
 
         # Check if we are near an obstacle if not blue = Avoid
-        if Nearobstacle(hownear):
-            if area > 2000:
-                print("We are near an obstacle but it is blue!")
-                    
-            else:
-                print("There is an obstacle in the way! Avoid!!")
-                Avoidobstacle()
+        #if Nearobstacle(hownear):
+        #    if area > 2000:
+        #        print("We are near an obstacle but it is blue!")
+        #            
+        #    else:
+        #        print("There is an obstacle in the way! Avoid!!")
+        #        Avoidobstacle()
 
             
-        elif (M["m00"] != 0) and (area > 100):
+        if (M["m00"] != 0) and (area > 100):
             print("Blue Ballon detected")
             cX = int(M["m10"] / M["m00"])
             cY = int(M["m01"] / M["m00"])
@@ -162,25 +164,31 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
             turn_amount = offset / 160
             MoveGirl(turn_amount, speed = 1)
             last_seen_balloon = time.time()
-       
-        
-        
         else:
-           # Explore
-            if time.time() - time_turned > 2:
-                turning_left = not turning_left
-                time_turned = time.time()
-                            
-            # Explore functions
-            if turning_left:                     
-                MoveGirl(-0.5, 0.5)
-                MoveGirl(0, 0.25)
-                print("Exploring to the left")
+            # no balloon here. But is there an obstacle?
+            if Nearobstacle(hownear) and (area<2000):
+                timeObstacleSeen = time.time() #record time that the obstacle was seen
 
+            if time.time() - timeObstacleSeen < 2: #we saw an obstacle less than 3 seconds ago
+                # do some avoidance stuff here
+                MoveGirl(-0.5, -1) #move back and turn a bit
             else:
-                MoveGirl(0.5, 0.5)
-                MoveGirl(0, 0.25)
-                print("Exploring to the right")
+                # no obstacle seen, so just explore
+                # Explore
+                if time.time() - time_turned > 2:
+                    turning_left = not turning_left
+                    time_turned = time.time()
+                                
+                # Explore functions
+                if turning_left:                     
+                    MoveGirl(0, 0.65)
+                    MoveGirl(-0.5, 0.75)
+                    print("Exploring to the left")
+
+                else:
+                    MoveGirl(0, 0.65)
+                    MoveGirl(0.5, 0.5)
+                    print("Exploring to the right")
 
              
             
